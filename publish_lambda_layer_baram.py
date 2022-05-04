682b3a400e6348844e2f5ee0b783ec4f50edf375
lks21c <lks21c@gmail.com>
2022-05-04 19:59:55 +0900

    Fix Bug

--- publish_lambda_layer_baram.py
+++ publish_lambda_layer_baram.py
@@ -1,5 +1,8 @@
+from baram.s3_manager import S3Manager
+from baram.kms_manager import KMSManager
 from baram.lambda_manager import LambdaManager
 
 if __name__ == '__main__':
+    sm = S3Manager('sli-dst-security', KMSManager().get_kms_arn('s3-hydra01-kms', False))
     lm = LambdaManager()
-    lm.publish_lambda_layer('baram')
+    lm.publish_lambda_layer('baram', 'sli-dst-security', sm)
