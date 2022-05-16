22927c0cb3e0bad4d21cfc20a1799ef64c6d1dcc
lks21c <lks21c@gmail.com>
2022-05-16 13:29:59 +0900

    add managers

---
+++ tests/test_ecr_manager.py
@@ -0,0 +1,22 @@
+from pprint import pprint
+
+import pytest
+
+from baram.ecr_manager import ECRManager
+
+
+@pytest.fixture()
+def ecr():
+    return ECRManager()
+
+
+def test_describe_image(ecr):
+    images = ecr.describe_images('sli-docker')
+    assert images
+    pprint(images)
+
+
+def test_list_images(ecr):
+    images = ecr.list_images('sli-docker')
+    assert images
+    pprint(images)
