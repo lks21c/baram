678dc1b8b1c9b11d234f2f1f5ea5630fc85ffd77
lks21c <lks21c@gmail.com>
2022-05-26 17:30:46 +0900

    Refactor ac

--- baram/async_crawler.py
+++ baram/async_crawler.py
@@ -6,28 +6,48 @@ import nest_asyncio
 
 class AsyncCrawler(object):
 
-    def crawl_urls(self, urls: list, header: dict = None) -> list:
+    def get_urls(self, urls: list, **kwargs: dict) -> list:
         '''
         Retruns crawled htmls from urls using async io.
 
         :param urls: http url list
-        :param header: http request header as dictionary
+        :param **kwargs: additional parameters.
         :return: html list
         '''
 
         try:
             loop = asyncio.get_event_loop()
-            return loop.run_until_complete(self._fetch_pages(urls, header))
+            return loop.run_until_complete(self._fetch_pages('GET', urls, **kwargs))
         except RuntimeError:
             nest_asyncio.apply()
-            return loop.run_until_complete(self._fetch_pages(urls, header))
+            return loop.run_until_complete(self._fetch_pages('GET', urls, **kwargs))
 
-    async def _fetch_pages(self, urls: list, header: dict):
-        async with aiohttp.ClientSession() as session:
-            return await asyncio.gather(
-                *[self._fetch_page(session, url, header) for url in urls], return_exceptions=True)
+    def post_urls(self, urls: list, **kwargs: dict) -> list:
+        try:
+            loop = asyncio.get_event_loop()
+            return loop.run_until_complete(self._fetch_pages('POST', urls, **kwargs))
+        except RuntimeError:
+            nest_asyncio.apply()
+            return loop.run_until_complete(self._fetch_pages('POST', urls, **kwargs))
 
-    async def _fetch_page(self, session, url: str, header: dict) -> str:
-        async with session.get(url, headers=header) as response:
+    async def _fetch_pages(self, method: str, urls: list, **kwargs: dict):
+        if method == 'GET':
+            async with aiohttp.ClientSession() as session:
+                return await asyncio.gather(
+                    *[self._fetch_get_page(session, url, **kwargs) for url in urls], return_exceptions=True)
+        elif method == 'POST':
+            async with aiohttp.ClientSession() as session:
+                return await asyncio.gather(
+                    *[self._fetch_post_page(session, url, **kwargs) for url in
+                      urls],
+                    return_exceptions=True)
+
+    async def _fetch_get_page(self, session, url: str, **kwargs: dict) -> str:
+        async with session.get(url, **kwargs) as response:
+            html = await response.text()
+            return html if response.status == 200 else None
+
+    async def _fetch_post_page(self, session, url: str,  **kwargs: dict) -> str:
+        async with session.post(url,  **kwargs) as response:
             html = await response.text()
             return html if response.status == 200 else None
